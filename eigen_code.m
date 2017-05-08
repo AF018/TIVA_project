fcca5f9e882163b5adff2d50c831ff4698a2cd47
Antoine Francois <a.francois962@laposte.net>
2017-05-08 13:24:24 +0200

    Ajout de photo pour la reconstruction

--- eigen_code.m
+++ eigen_code.m
@@ -1,8 +1,13 @@
 %% Utilisation du code Github, prise en main pour application aux bases de donnees
 
+% faces94 tres long a charger, contrairement a att_data
+% la reconstruction d'une photo lambda marche tres bien pour faces94 a
+% partir de 1500 visages utilises
+% Resultats assez moyens avec AT&T par contre
+
 % Fonction pour aller lire les images dans la base de données
-%[X y w h] = read_images([pwd '/att_data']);
-[X y w h] = read_images([pwd '/faces94']);
+[X y w h] = read_images([pwd '/att_data']);
+%[X y w h] = read_images([pwd '/faces94']);
 
 % X est la matrice obtenue par concaténation des images transformées en
 % vecteurs
@@ -15,7 +20,7 @@
 % de gris
 figure;
 hold on;
-title('Eigenfaces (AT&T Facedatabase)');
+title('Eigenfaces');
 for i=1:min(16,n)
     subplot(4,4,i);
     eigenface_i = toGrayscale(W(:,i), w, h);
@@ -27,12 +32,14 @@ end
 
 % Appel de l'image a reconstruire
 %Q = X(1,:); % premiere image de la base de donnees
-Q_rgb = double(imread('test_2.jpg')); % Pour faces94
-%Q_rgb = double(imread('test_1.jpg')); % Pour At&T
+%Q_rgb = double(imread('test_2.jpg')); % Pour faces94
+Q_rgb = double(imread('test_1.jpg')); % Pour At&T
 Q = 0.2126*Q_rgb(:,:,1)+0.7152*Q_rgb(:,:,2)+0.0722*Q_rgb(:,:,3);
 Q = reshape(Q,1,w*h);
 
 % Reconstruction
+% Le nombre d'eigenvectors utilise est a modifier en fonction de la base de
+% donnees : on peut en rajouter avec faces94
 steps = 10:20:min(n,320);
 figure;
 hold on;
--- fisher_code.m
+++ fisher_code.m
@@ -1,10 +1,18 @@
 %% Utilisation du code Github, prise en main pour application aux bases de données
 
+% faces94 tres long a charger, contrairement a att_data
+% Les resultats sont plus significatifs avec AT&T, on reconnait certaines
+% caracteristiques des visages contrairement a faces94.
+% Ceci peut s'expliquer par le fait que la LDA permet de souligner ce qui
+% distingue les classes. Or, avec beaucoup plus de personnes, il est
+% difficile de comprendre a l'oeil nu le contenu des fisherfaces car les
+% differences sont plus subtiles
+
 % Fonction pour aller lire les images dans la base de données
-db_name = 'AT&T Database';
-[X y w h] = read_images([pwd '/att_data']);
-%db_name = 'faces94 Database';
-%[X y w h] = read_images([pwd '/faces94']);
+%db_name = 'AT&T Database';
+%[X y w h] = read_images([pwd '/att_data']);
+db_name = 'faces94 Database';
+[X y w h] = read_images([pwd '/faces94']);
 
 % X est la matrice obtenue par concaténation des images transformées en
 % vecteurs
@@ -27,4 +35,26 @@ for i=1:min(16,length(c)-1)
   %colormap(jet(256));
   colormap(gray);
   title(sprintf('Fisherface #%i', i));
+end
+
+% Appel de l'image a reconstruire
+%Q = X(1,:); % premiere image de la base de donnees
+Q_rgb = double(imread('test_2.jpg')); % Pour faces94
+%Q_rgb = double(imread('test_1.jpg')); % Pour At&T
+Q = 0.2126*Q_rgb(:,:,1)+0.7152*Q_rgb(:,:,2)+0.0722*Q_rgb(:,:,3);
+Q = reshape(Q,1,w*h);
+
+% Projection d'un visage sur chacune des fisherfaces
+steps = 1:min(16, length(c)-1) ;
+figure;
+hold on ;
+title(sprintf('Fisherfaces Reconstruction %s',db_name)) ;
+for i = 1:min(16,length(steps))
+    subplot(4,4,i);
+    numEv = steps(i) ;
+    P = project(W(:,numEv),X(1,:),mu);
+    R = reconstruct(W(:,numEv),P,mu);
+    comp = toGrayscale(R,w,h);
+    imshow(comp);
+    title(sprintf('Fisherface #%i',numEv));
 end
